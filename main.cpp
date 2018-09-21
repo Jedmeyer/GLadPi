48038841811412a19ba23c922b4a1dcbf2c2830f
Jason Dickmeyer <jedmeyer@stolaf.edu>
2018-09-21 14:28:53 -0500

    automated file detection. Currently supports .wav's and .mp3's. I don't think files need to be named as a number anymore.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<wiringPi.h>
+#include<dirent.h>
 #include<unistd.h>
 #include<iostream>
 #include<stdlib.h>
@@ -6,29 +7,56 @@
 #include<time.h>
 #include<string.h>
 #include<math.h>
+#include<string>
+#include<string.h>
+#include<vector>
 
 
 #define BITS 8
 
 
 using namespace std;
-int numberOfSongs = 37;
+int numberOfSongs = 0;
+char * path;
+vector<string> songList;
+
+
+string getFileExt(const string& s) {
+
+   size_t i = s.rfind('.', s.length());
+   if (i != string::npos) {
+      return(s.substr(i+1, s.length() - i));
+   }
+   return("");
+}
+
+
 int play_audio()
 {
-	char *path = new char[500];
-	int randNum  = rand()%numberOfSongs +1;
+	// Build the path for the audio list.
+	
+
+	string formedPath(path);
+
+	int randNum = rand()%numberOfSongs + 1;
+	string songExt(getFileExt(songList[randNum]));
+	char *playCall = new char[500];
 
 
-	if(randNum<21)
-	snprintf(path,500,"mpg123 /home/pi/dev/gladpi/Apeture/%d.mp3",randNum); 
-    else
-    snprintf(path,500,"aplay /home/pi/dev/gladpi/Apeture/%d.wav",randNum);
-	cout << path << endl;
- 
-	int ret = 0;
+
+
+	if(strcmp(songExt.c_str(),"mp3") == 0 )
+	snprintf(playCall,500,"mpg123 %s",songList[randNum].c_str()); 
+    else if(strcmp(songExt.c_str(), "wav") == 0 )
+    snprintf(playCall,500,"aplay %s", songList[randNum].c_str());
 	 
-	system(path);
-	delete(path);
+ 	
+  
+	int ret = 0;
+	
+	cout << "Calling bash command: " << playCall << endl;
+	system(playCall);
+	delete(playCall);
 	return 0; 
   
 
@@ -38,24 +66,81 @@ int play_audio()
 
 
 
+void initialize_audio(){
+	
+	DIR *d;
+	struct dirent *dir;
+	d = opendir("./Apeture");
+	string formedPath(path);
+    formedPath = formedPath + "/Apeture";
+	strcpy(path, formedPath.c_str());
+
+
+	if (d)
+	{
+		while((dir = readdir(d)) != NULL)
+		{
+			
+				string formedPath(path);
+				string songName(dir->d_name);
+				formedPath = formedPath + "/" + songName;
+
+				songList.push_back(formedPath);
+				printf("%s\n", formedPath.c_str());
+				numberOfSongs++;
+
+
+			
+
+		}
+		closedir(d);
+		cout << "Number of Songs: " << numberOfSongs << endl;
+
+	}
+
+
+
+
+
+
+
+
+
+}
+
+
 int main(){
+
+	path = new char[200];
+	for(int i =0; i < 200; i++)
+		path[i] == '\0';
+	getcwd(path, 200);
+	printf("%s\n", path);
+	string s(path); 
+	s = s + "/Apeture/";
+	cout << endl << s;
+
+
+	initialize_audio(); 
 	system("espeak 'Alert. glad pi is now online.'");
 	srand(5);
+	play_audio();
+
+
+
+	
 	wiringPiSetup(); 
 	pinMode(7,INPUT);
 	while (true){
 	
 		
-		if(digitalRead(7) >0){
+		if(digitalRead(7) >0)
 			cout << "I DETECT YOU\n";
 			play_audio(); 
 			usleep(10000000);
-		}
+	
 
-	}
+		
 return 0;
-
-
-
 }
 
